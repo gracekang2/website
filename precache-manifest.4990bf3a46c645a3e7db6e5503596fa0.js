8df09adc8bca04f3f02ad6743e47a5a51e490a9b
Grace Kang <g8kang@uwaterloo.ca>
2020-12-18 18:30:05 -0500

    new build

---
+++ precache-manifest.4990bf3a46c645a3e7db6e5503596fa0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3883109870dfdbab47e5604993ad7bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dff8f5a3a831db6a4820",
+    "url": "/static/css/main.feda3ce7.chunk.css"
+  },
+  {
+    "revision": "5f79e3f672df8fe50db9",
+    "url": "/static/js/2.6749dbef.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6749dbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dff8f5a3a831db6a4820",
+    "url": "/static/js/main.86e7cee7.chunk.js"
+  },
+  {
+    "revision": "0c352a0e60badd2e6729",
+    "url": "/static/js/runtime-main.963817d1.js"
+  },
+  {
+    "revision": "ed94c96ff6e3824b686e1badffc752cd",
+    "url": "/static/media/blob.ed94c96f.svg"
+  },
+  {
+    "revision": "df04e6ca6e071cec4a493274f263fffc",
+    "url": "/static/media/mobile_blob.df04e6ca.svg"
+  },
+  {
+    "revision": "cc9187067ff90b1173f22fc363c6e831",
+    "url": "/static/media/sanfran.cc918706.jpg"
+  },
+  {
+    "revision": "2ce107be7163246e1f6d812565464a6e",
+    "url": "/static/media/toronto.2ce107be.jpg"
+  }
+]);
