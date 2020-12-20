87be301f8710a0aa353b4a01e2f98af17a5d84f3
Grace Kang <g8kang@uwaterloo.ca>
2020-12-19 19:22:31 -0500

    Changed projects section

--- src/Components/ProjectTile.js
+++ src/Components/ProjectTile.js
@@ -1,15 +1,26 @@
 import React from "react";
 
 export default class ProjectTile extends React.Component {
+    state = {
+        desc: false
+    };
+
+    OnTileClick = (event) => {
+        this.setState((prev) => ({
+            desc: !prev.desc
+        }));
+    }
+
     render() {
         return (
-        <div className="projectTile" style={{backgroundColor: this.props.color}} onClick={this.OnTileClick}>
+        <div className="projectTile" style={{backgroundColor: this.props.color, color: this.props.textColor , border: "1px solid " + this.props.borderColor}} onClick={this.OnTileClick}>
             <div className="projectDescriptionBlock">
                 <h3>{this.props.title}</h3>
+                {this.props.image && !this.state.desc ? <img src={this.props.image} className="projectTileImg"/> : <></>}
                 {this.props.tech.map(t => {
                     return <span className="techBadge" style={{backgroundColor: this.props.badgeColor}}>{t}</span>
                 })}<br/>
-                {this.props.description}
+                {this.state.desc || !this.props.image ? this.props.description : <></>}
             </div>
         </div>
         );
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,39 +4,64 @@ import ProjectTile from "./ProjectTile";
 export default class Projects extends React.Component {
   render() {
     return (
-      <div className="projectsRow">
-          <ProjectTile color="#a6e0de" badgeColor="#3ebdc6" title="MyBCP" tech={["C#", "SQL", "ASP.NET", "jQuery"]} description={
-            <>
-                A web tool that displays a customized and easily digestible summary of an
-                employee's Business Continuity Plan, including emergency contact information and alternative office info. 
-                It is estimated that upwards of <b> 2000 Toronto employees</b> will be using this tool.<br/>
-                <i>(Made for Scotiabank.)</i><br/>
-          </>
-        }/>
-          <ProjectTile color="#bebccb" badgeColor="#8d89a3" title="Bulk Fund Import" tech={["C#", "SQL", "ASP.NET", "jQuery"]} description={
+      <>
+        <div className="projectsRow">
+            <ProjectTile color="#46aea3" badgeColor="#75e5e6" title="EcoStore" tech={["Node.js", "Express.js", "MongoDB", "Microsoft Azure"]} description={
               <>
-                A tool to automate tedious manual
-                fund tracking. Designed to process multiple spreadsheets with over 120 000+ rows of fund data, and make the
-                necessary updates to the bank's database. Built 
-                to be incredibly user friendly, with a drag and drop file upload
-                mechanism, and a calendar displaying a summary of the updates and inserts made
-                on each day.<br/>
-                <i>(Made for Scotiabank.)</i><br/>
-              </>
-          }
+                  An iOS app, created at HackTheNorth in September 2019, that uses image recognition to gamify the art of proper recycling. By continuing 
+                  to properly recycle, users can restore their virtual ecosystems.<br/>
+            </>
+          } image="./Images/ecostore.jpg"
           />
-          <ProjectTile color="#cbe3b3" badgeColor="#b7df69" title="UserMatrix" tech={["C#", "SQL", "ASP.NET", "jQuery"]} description={
+            <ProjectTile color="#bebccb" badgeColor="#8d89a3" title="Live ASL Alphabet Translator" tech={["Python", "fastai + PyTorch", "OpenCV"]} description={
+                <>
+                  A command line program that takes in live video feed from a user's webcam, and recognizes + translates static ASL alphabet signs. This program 
+                  is built on a deep learning model, trained using a homemade dataset, which achieved over 99% accuracy in training. <br/>
+                </>
+            } image="./Images/asl.gif"
+            />
+            <ProjectTile color="#ffffff" badgeColor="#ffb5bf" textColor="black" borderColor="black" title="BiQuadris" tech={["C++", "OOP"]} description={
+                <>
+                  A command line version of Tetris, made in C++. This game was a school project completed with 2 other members, and utilized a variety of OOP design patterns. 
+                  Achieved a final grade of 98%.<br />
+                </>
+            } image="./Images/biquadris.jpeg"
+            />
+        </div>
+        <div className="projectsRow">
+            <ProjectTile color="#a6e0de" badgeColor="#3ebdc6" title="MyBCP" tech={["C#", "SQL", "ASP.NET", "jQuery"]} description={
               <>
-                A <b>JavaScript library</b> for internal tool development within Scotiabank.r
-                A library for generating highly customizable,{" "}
-                <b>2-dimensional</b> batch edit modals to edit bank Users against another
-                dimension (e.g. applications). The modal was designed to be highly customizable for the
-                types of users, the dimension against which users would be edited,
-                adding capabilities, etc. <br />
-                <i>(Made for Scotiabank.)</i><br/>
-              </>
+                  A web tool that displays a customized and easily digestible summary of an
+                  employee's Business Continuity Plan, including emergency contact information and alternative office info. 
+                  It is estimated that upwards of <b> 2000 Toronto employees</b> will be using this tool.<br/>
+                  <i>(Made for Scotiabank.)</i><br/>
+            </>
           }/>
-      </div>
+            <ProjectTile color="#8d89a3" badgeColor="#bebccb" title="Bulk Fund Import" tech={["C#", "SQL", "ASP.NET", "jQuery"]} description={
+                <>
+                  A tool to automate tedious manual
+                  fund tracking. Designed to process multiple spreadsheets with over 120 000+ rows of fund data, and make the
+                  necessary updates to the bank's database. Built 
+                  to be incredibly user friendly, with a drag and drop file upload
+                  mechanism, and a calendar displaying a summary of the updates and inserts made
+                  on each day.<br/>
+                  <i>(Made for Scotiabank.)</i><br/>
+                </>
+            }
+            />
+            <ProjectTile color="#cbe3b3" badgeColor="#b7df69" title="UserMatrix" tech={["C#", "SQL", "ASP.NET", "jQuery"]} description={
+                <>
+                  A <b>JavaScript library</b> for internal tool development within Scotiabank.r
+                  A library for generating highly customizable,{" "}
+                  <b>2-dimensional</b> batch edit modals to edit bank Users against another
+                  dimension (e.g. applications). The modal was designed to be highly customizable for the
+                  types of users, the dimension against which users would be edited,
+                  adding capabilities, etc. <br />
+                  <i>(Made for Scotiabank.)</i><br/>
+                </>
+            }/>
+        </div>
+      </>
     );
   }
 }
